c726d357561533c8e7891147d64362d25cb7e212
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-31 14:04:25 -0400

    practice

--- while-loop.sh
+++ while-loop.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# a=0
+# while [ $a -lt 10 ]
+# do
+#     echo $a
+#     a='expr $a + 1'
+# done
+
+
+
 a=0
+
 while [ $a -lt 10 ]
 do
-    echo $a
-    a='expr $a + 1'
+   echo $a
+   a=`expr $a + 1`
 done
