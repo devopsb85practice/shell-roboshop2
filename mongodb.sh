a17783ebd35c2e2034f76911f77f5a0997d81363
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-30 21:09:30 -0400

    practice

--- mongodb.sh
+++ mongodb.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="/$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-echo "script started executing at $(date)"| tee -a $LOG_FILE
-if [ "$USERID" -ne 0 ]
-then   
-    echo -e "$R you are not running with root access, please run with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
-fi
-VALIDATE(){
-if [ "$1" -eq 0 ]
-then
-    echo -e "$2 is $G success $N" | tee -a $LOG_FILE
-else
-    echo -e "$2 is $R failure $N" | tee -a $LOG_FILE
-    exit 1
-fi
-}
+source ./common.sh
+app_name=mongodb
+check_root
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 VALIDATE $? "copying the repo file" 
 dnf install mongodb-org -y &>>$LOG_FILE
@@ -37,3 +14,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
 VALIDATE $? "editing mongo.conf file" 
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restarting mongodb"
+print_time
